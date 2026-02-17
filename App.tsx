5f286239fbe3e6fa69c1dcbc2ed3a9fdbc83ef61
jackson-girard <jackson.girard@coyotes.usd.edu>
2026-02-17 09:27:47 -0600

    Added timer state, laps, and refs

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Platform, TouchableOpacity } from 'react-native';
 
+type Lap = {
+  id: string;
+  time: number;
+}
 export default function App() {
   return (
     <View style={styles.container}>
